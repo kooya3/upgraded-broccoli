aadd390f0cbcfd4c93860ea67a5bd45b1903a5bc
Elyees Tatua <elyeestatua@gmail.com>
2024-01-04 13:34:19 +0300

    ops(app): Working on the img config

--- cloud/dropbox-clone/components/Header.tsx
+++ cloud/dropbox-clone/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,6 +6,13 @@ function Header() {
   return (
     <header className="flex items-center justify-between">
         <Link href="/">
+        <Image
+            src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
+            alt="logo"
+            className="invert"
+            height={50}
+            width={50}
+          />
         <h1 className="font-bold text-xl">Dropbox</h1>
         </Link>
     </header>
