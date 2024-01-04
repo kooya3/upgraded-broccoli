70e29e933efa34453074518502b7c8f10a906433
Elyees Tatua <elyeestatua@gmail.com>
2024-01-04 13:06:53 +0300

    ops(app): Declaring the userbutton component

--- cloud/dropbox-clone/app/page.tsx
+++ cloud/dropbox-clone/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Dropbox clone</h1>
+
+      <UserButton />
     </main>
   )
 }
