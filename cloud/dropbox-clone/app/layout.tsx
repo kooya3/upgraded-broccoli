c3d3e42b90673d78342a352c2b0e857d1ee9cd34
Elyees Tatua <elyeestatua@gmail.com>
2024-01-04 13:16:30 +0300

    ops(app): creating the header component

--- cloud/dropbox-clone/app/layout.tsx
+++ cloud/dropbox-clone/app/layout.tsx
@@ -18,7 +18,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Header />
+          
+          {children}
+          
+          </body>
       </html>
 
     </ClerkProvider>
