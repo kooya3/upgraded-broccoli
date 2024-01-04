3db94f61b674d4b8054985945768743acf242d33
Elyees Tatua <elyeestatua@gmail.com>
2024-01-04 15:27:25 +0300

    ops(header): Working on the header component

--- cloud/dropbox-clone/components/Header.tsx
+++ cloud/dropbox-clone/components/Header.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -5,7 +6,8 @@ import React from 'react'
 function Header() {
   return (
     <header className="flex items-center justify-between">
-        <Link href="/">
+        <Link href="/" className="flex items-center space-x-3">
+            <div className="bg-[#0160fe] w-fit">
         <Image
             src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
             alt="logo"
@@ -13,8 +15,16 @@ function Header() {
             height={50}
             width={50}
           />
+          </div>
         <h1 className="font-bold text-xl">Dropbox</h1>
         </Link>
+
+        {/* Theme toggler */ }
+
+        <UserButton afterSignOutUrl="/" />
+        <SignedOut>
+            <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+        </SignedOut>
     </header>
   )
 }
