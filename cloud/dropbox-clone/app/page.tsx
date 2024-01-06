cadad628f5eb68172833879f3039476c16621e7b
Elyees Tatua <elyeestatua@gmail.com>
2024-01-06 23:56:34 +0300

    ops(page): update

--- cloud/dropbox-clone/app/page.tsx
+++ cloud/dropbox-clone/app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dropbox clone</h1>
+      <h1>Dropbox</h1>
 
       <UserButton />
     </main>
