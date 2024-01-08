3f52cc2364d0c88376e1949f42c0bf8fe94f3466
Elyees Tatua <elyeestatua@gmail.com>
2024-01-09 00:01:34 +0300

    ops(home): Working on the home section of the page

--- cloud/dropbox-clone/app/page.tsx
+++ cloud/dropbox-clone/app/page.tsx
@@ -1,11 +1,19 @@
-import { UserButton } from "@clerk/nextjs"
-
-export default function Home() {
+function dashboard() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dropbox</h1>
+    <main className="">
+      <div>
+    <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5 font-mono">
+      <h1 className="text-5xl font-bold">
+        Welcome to DropBox. <br />
+        <br />
+        Storing everything for you and your business needs. All in one place
+      </h1>
+    </div>
+    </div>
 
-      <UserButton />
+    <p className="text-center font-bold text-xl pt-5">Disclaimer</p> 
     </main>
-  )
+  );
 }
+
+export default dashboard
