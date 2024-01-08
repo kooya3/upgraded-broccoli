52e5127e4d689f2db4b1d83b36a5e0fb9382936f
Elyees Tatua <elyeestatua@gmail.com>
2024-01-09 00:17:51 +0300

    ops(Home): Working on the home section

--- cloud/dropbox-clone/app/page.tsx
+++ cloud/dropbox-clone/app/page.tsx
@@ -2,7 +2,7 @@ function dashboard() {
   return (
     <main className="">
       <div>
-    <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5 font-sans">
+    <div className="p-10 flex flex-col bg-[#cccaca] dark:bg-slate-800 text-white space-y-5 font-sans">
       <h1 className="text-5xl font-bold">
         Welcome to DropBox. <br />
         <br />
@@ -12,6 +12,9 @@ function dashboard() {
     </div>
 
     <p className="text-center font-bold text-xl pt-5">Disclaimer</p> 
+    <p className="text-center font-light p-2">
+      This project is made for informational and educational purposes only. 
+    </p>
     </main>
   );
 }
