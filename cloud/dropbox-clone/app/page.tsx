460eb78d9662c7fb595d3afdca76bc91a460a5dc
Elyees Tatua <elyeestatua@gmail.com>
2024-01-09 00:42:28 +0300

    ops(Dashboard): Started working on the dashboard

--- cloud/dropbox-clone/app/page.tsx
+++ cloud/dropbox-clone/app/page.tsx
@@ -1,3 +1,6 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 function dashboard() {
   return (
     <main className="">
@@ -8,8 +11,18 @@ function dashboard() {
         <br />
         Storing everything for you and your business needs. All in one place
       </h1>
+
+      <p className="pb-20">
+        Enhance your personal storage. Offering a simple and efficient way to upload, organize, and access files from anywhere. 
+      </p>
+
+      <Link href="/dashboard">
+      Try it for free!
+      <ArrowRight className="" />
+      </Link>
     </div>
     </div>
+
 
     <p className="text-center font-bold text-xl pt-5">Disclaimer</p> 
     <p className="text-center font-light p-2">
