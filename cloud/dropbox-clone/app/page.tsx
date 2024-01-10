3c981915289368de2640c4cf9b56efc3f307ae4b
Elyees Tatua <elyeestatua@gmail.com>
2024-01-10 19:27:54 +0300

    ops(update): Update on the project

--- cloud/dropbox-clone/app/page.tsx
+++ cloud/dropbox-clone/app/page.tsx
@@ -7,9 +7,10 @@ export default function Home() {
       <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800">
         <div className="p-10 font-sans flex flex-col bg-[#1E1919] dark:bg-slate-800 text-[#FFFFFF] space-y-5 space-x-4">
           <h1 className="m-lg text-5xl font-bold">
-            Welcome to Dropbox. <br /><br />
-            Storing everything for you and your business needs. All in one place
-          </h1>
+            Welcome to Dropbox. <br />
+            Storing everything for you and your business needs. All in one place.
+            
+      </h1>
 
           <p className="mt-lg m-lg pb-20 space-y-3">
             Enhance your personal storage with Dropbox, offering a simple and
