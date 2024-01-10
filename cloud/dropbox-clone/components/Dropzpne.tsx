b8973fdb3fef52327c7018daeec8a12cb7da04df
Elyees Tatua <elyeestatua@gmail.com>
2024-01-10 14:40:01 +0300

    ops(dropzone): Restructured the format to implement use client directive

--- cloud/dropbox-clone/components/Dropzpne.tsx
+++ cloud/dropbox-clone/components/Dropzpne.tsx
@@ -1,14 +1,18 @@
-"use client";
+import Dropzone from "react-dropzone";
 
-import Dropzone from 'react-dropzone'
+function Dropzone() {
+  return (
+    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+      {({getRootProps, getInputProps}) => (
+        <section>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+        </section>
+      )}
+    </Dropzone>
+  )
+}
 
-<Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-  {({getRootProps, getInputProps}) => (
-    <section>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-    </section>
-  )}
-</Dropzone>
+export default Dropzone
