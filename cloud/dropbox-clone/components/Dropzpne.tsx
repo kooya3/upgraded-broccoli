0db4fd08ca0f9990ac7b039baabc7b77ec346586
Elyees Tatua <elyeestatua@gmail.com>
2024-01-10 14:57:56 +0300

    ops(dropzone): Working on the drag n drop feature as weel as implementing more props

--- cloud/dropbox-clone/components/Dropzpne.tsx
+++ cloud/dropbox-clone/components/Dropzpne.tsx
@@ -3,12 +3,25 @@
 import DropzoneComponent from "react-dropzone";
 
 function Dropzone() {
+  // max file size 20MB
+  const maxSize = 20971520;
+
   return (
-    <DropzoneComponent onDrop={acceptedFiles => console.log(acceptedFiles)}>
-      {({getRootProps, getInputProps}) => (
+    <DropzoneComponent 
+    minSize={0}
+    maxSize={maxSize}
+      onDrop={acceptedFiles => console.log(acceptedFiles)}>
+      {({ getRootProps, getInputProps, isDragActive, isDragReject, fileRejections }) => {
+        const isFileTooLarge = 
+        fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
+
+        return (
+
+     
         <section>
           <div {...getRootProps()}>
             <input {...getInputProps()} />
+            {}
             <p>Drag 'n' drop some files here, or click to select files</p>
           </div>
         </section>
