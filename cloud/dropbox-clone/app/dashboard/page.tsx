cde57f5346db863ed9898e8c6cfa9cf768a30619
Elyees Tatua <elyeestatua@gmail.com>
2024-01-11 14:01:40 +0300

    ops(page): update

--- cloud/dropbox-clone/app/dashboard/page.tsx
+++ cloud/dropbox-clone/app/dashboard/page.tsx
@@ -1,16 +1,40 @@
 import Dropzone from "@/components/Dropzpne";
-import { auth } from "@clerk/nextjs"
+import TableWrapper from "@/components/table/TableWrapper";
+import { db } from "@/firebase";
+import { FileType } from "@/typings";
+import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 
-
-
-function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
 
+  const docsResults = await getDocs(collection(db, "users", userId!, "files"));
+  const skeletonFiles: FileType[] = docsResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
+  console.log(skeletonFiles);
+
   return (
-    <div>
+    <div className="border-t">
       <Dropzone />
+
+      <section className="container space-y-5">
+        <h2 className="font-bold">All Files</h2>
+
+        <div>
+          <TableWrapper 
+          skeletonFiles = {skeletonFiles} />
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
