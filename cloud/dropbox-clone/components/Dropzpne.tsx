e7f0bb8e7761731fa55e6de99807692d4f6daccc
Elyees Tatua <elyeestatua@gmail.com>
2024-01-12 23:33:27 +0300

    ops(dropzone): update

--- cloud/dropbox-clone/components/Dropzpne.tsx
+++ cloud/dropbox-clone/components/Dropzpne.tsx
@@ -3,17 +3,24 @@
 import { db, storage } from "@/firebase";
 import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
-import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useState } from "react";
 import DropzoneComponent from "react-dropzone";
+import toast from "react-hot-toast";
 
 function Dropzone() {
   const [loading, setLoading] = useState(false);
   const { isLoaded, isSignedIn, user } = useUser();
 
   const onDrop = (acceptedFiles: File[]) => {
-    acceptedFiles.forEach(file => {
+    acceptedFiles.forEach((file) => {
       const reader = new FileReader();
 
       reader.onabort = () => console.log("file reading was aborted");
@@ -22,7 +29,7 @@ function Dropzone() {
         await uploadPost(file);
       };
       reader.readAsArrayBuffer(file);
-    }); 
+    });
   };
 
   const uploadPost = async (selectedFile: File) => {
@@ -30,9 +37,10 @@ function Dropzone() {
     if (!user) return;
 
     setLoading(true);
-    
-     // users/user12345/files
-     const docRef = await  addDoc(collection(db, "users", user.id, "files"), {
+    const toastId = toast.loading("Uploading...");
+
+    // addDoc -> users/user12345/files
+    const docRef = await addDoc(collection(db, "users", user.id, "files"), {
       userId: user.id,
       filename: selectedFile.name,
       fullName: user.fullName,
@@ -40,60 +48,63 @@ function Dropzone() {
       timestamp: serverTimestamp(),
       type: selectedFile.type,
       size: selectedFile.size,
-     })
+    });
 
- 
-     const imageRef = ref(storage, `users/${user.id}/files/${docRef.id}`);
+    const imageRef = ref(storage, `users/${user.id}/files/${docRef.id}`);
 
-     uploadBytes(imageRef, selectedFile).then(async (snapshot) => {
-       const downloadURL = await getDownloadURL(imageRef);
- 
-       await updateDoc(doc(db, "users", user.id, "files", docRef.id), {
-         downloadURL: downloadURL,
-       });
-     });
- 
-     
-     setLoading(false);
-   };
- 
-   // max file size 20MB
-   const maxSize = 20971520;
- 
+    uploadBytes(imageRef, selectedFile).then(async (snapshot) => {
+      const downloadURL = await getDownloadURL(imageRef);
+
+      await updateDoc(doc(db, "users", user.id, "files", docRef.id), {
+        downloadURL: downloadURL,
+      });
+    });
+
+    toast.success("Uploaded Successfully", {
+      id: toastId,
+    });
+    setLoading(false);
+  };
+
+  // max file size 20MB
+  const maxSize = 20971520;
 
   return (
-    <DropzoneComponent 
-    minSize={0}
-    maxSize={maxSize}
-      onDrop={onDrop}>
-      {({ getRootProps, getInputProps, isDragActive, isDragReject, fileRejections }) => {
-        const isFileTooLarge = 
-        fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
+    <DropzoneComponent minSize={0} maxSize={maxSize} onDrop={onDrop}>
+      {({
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        isDragReject,
+        fileRejections,
+      }) => {
+        const isFileTooLarge =
+          fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
 
         return (
-
-     
-        <section className="m-4">
-          <div 
-          {...getRootProps()}
-          className={cn(
-            "w-full h-[20vh] flex justify-center items-center p-5 border border-dashed rounded-lg text-center ",
-            isDragActive ? "bg-[#035FFE] text-white animate-pulse" : "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400"
-          )}
-          >
-            <input {...getInputProps()} />
-            {!isDragActive && "Click here or drop a file to upload!"}
-            {isDragActive && !isDragReject && "Drop to upload this file!"}
-            {isDragReject && "File type not accepted, sorry!"}
-            {isFileTooLarge && (
-              <div className="text-danger mt-2">File is too large.</div>
-            )}
-          </div>
-        </section>
-      );
-    }}
+          <section className="m-4">
+            <div
+              {...getRootProps()}
+              className={cn(
+                "w-full h-[20vh] flex justify-center items-center p-5 border border-dashed rounded-lg text-center",
+                isDragActive
+                  ? "bg-[#035FFE]  text-white animate-pulse"
+                  : "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400"
+              )}
+            >
+              <input {...getInputProps()} />
+              {!isDragActive && "Click here or drop a file to upload!"}
+              {isDragActive && !isDragReject && "Drop to upload this file!"}
+              {isDragReject && "File type not accepted, sorry!"}
+              {isFileTooLarge && (
+                <div className="text-danger mt-2">File is too large.</div>
+              )}
+            </div>
+          </section>
+        );
+      }}
     </DropzoneComponent>
   );
 }
 
-export default Dropzone
+export default Dropzone;
