798b6775f901b250bf6051989963869dba271178
Elyees Tatua <elyeestatua@gmail.com>
2024-01-12 23:38:27 +0300

    ops(header): Update on the header file

--- cloud/dropbox-clone/components/Header.tsx
+++ cloud/dropbox-clone/components/Header.tsx
@@ -1,34 +1,35 @@
-import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { ThemeToggler } from './ThemeToggler'
+import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeToggler } from "./ThemeToggler";
 
 function Header() {
   return (
     <header className="flex items-center justify-between">
-        <Link href="/" className="flex items-center space-x-3">
-            <div className="bg-[#0160fe] w-fit">
-        <Image
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="bg-[#0160FE] w-fit">
+          <Image
             src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
             alt="logo"
             className="invert"
             height={50}
             width={50}
           />
-          </div>
+        </div>
         <h1 className="font-bold text-xl">MyBox</h1>
-        </Link>
+      </Link>
 
-        {/* Theme toggler */ }
+      <div className="px-5 flex space-x-2 items-center">
         <ThemeToggler />
 
         <UserButton afterSignOutUrl="/" />
+
         <SignedOut>
-            <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+          <SignInButton afterSignInUrl="/dashboard" mode="modal" />
         </SignedOut>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
