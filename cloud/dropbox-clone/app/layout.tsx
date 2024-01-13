48f8699ff15830e1eb870055a84dd022091009aa
Elyees Tatua <elyeestatua@gmail.com>
2024-01-13 13:50:38 +0300

    ops(layout): Updating the position of toast

--- cloud/dropbox-clone/app/layout.tsx
+++ cloud/dropbox-clone/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           
           {children}
 
-          <Toaster />
+          <Toaster position="top-center" />
           </ThemeProvider>
           
           </body>
